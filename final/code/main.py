8c5cc1a52e8f46c21696190a3d6e216521eb2df0
David Hoeppner <dhoeppne@ualberta.ca>
2019-10-25 21:35:49 -0600

    finally done

---
+++ final/code/main.py
@@ -0,0 +1,130 @@
+# import external libraries
+import sys, os, re
+from tempfile import mkstemp
+from shutil import move
+from os import fdopen, remove
+# import my files
+from models import HMM, Brill, StanfordModel
+
+def replace(file_path, pattern, subst):
+    #Create temp file
+    fh, abs_path = mkstemp()
+    with fdopen(fh,'w') as new_file:
+        with open(file_path) as old_file:
+            for line in old_file:
+                new_file.write(line.replace(pattern, subst))
+    #Remove original file
+    remove(file_path)
+    #Move new file
+    move(abs_path, file_path)
+
+def prepareNLTK(directory):
+    tagList = []
+    sentenceList = []
+    with open(directory, "r") as file:
+        for line in file:
+            split = line.split(" ")
+
+            if len(split) > 1:
+                sentenceList.append((split[0], split[1][:-1])) # delete new line characters
+            else:
+                tagList.append(sentenceList)
+                sentenceList = []
+
+    return tagList
+
+def testDirectory(model, files):
+    output = []
+    for testFile in files:
+        o = model.test(testFile)
+        if o is not None:
+            output.append(o)
+
+    return output
+
+def main(mode, directory):
+
+    preFiles = os.listdir(directory)
+    files = []
+    for file in preFiles:
+        if ".txt" in file:
+            files.append(file)
+
+    try:
+        remove("./results.txt")
+        f = open("results.txt", "w+")
+        f.close()
+    except:
+        print("Could not create results file")
+
+    for file in files:
+        for wrong in files:
+            # prep file based on mode
+            if mode == "--stanford":
+                replace(os.path.join(directory, wrong), " ", "/")
+            elif mode == "--nltk":
+                replace(os.path.join(directory, wrong), "/", " ")
+
+        if "Train" in file:
+            if mode == "--stanford":
+
+                fileName = "st_" + file.split(".")[0] + ".tagger"
+                model = StanfordModel(fileName, os.path.join(directory, file))
+                model.train()
+
+                directoriedFiles = []
+                for f in files:
+                    directoriedFiles.append(os.path.join(directory, f))
+
+                testDirectory(model, directoriedFiles)
+
+            elif mode == "--nltk":
+                tagList = prepareNLTK(os.path.join(directory, file))
+
+                fileName = "nltk1.txt" if "Domain1" in directory else "nltk2.txt"
+
+                modelHMM = HMM(directory=directory)
+                trainedHMM = modelHMM.train(text=tagList)
+                modelBrill = Brill(directory=directory, tagger=trainedHMM)
+                modelBrill.train(text=tagList)
+
+                testList = []
+                fileList = []
+                for testFile in files:
+                    if "Test" in testFile:
+                        testList.append(prepareNLTK(os.path.join(directory, testFile)))
+                        fileList.append(testFile)
+
+                hmmAcc = testDirectory(modelHMM, testList)
+                brillAcc = testDirectory(modelBrill, testList)
+
+                with open("results.txt", "a") as results:
+                    i = 0
+                    for acc in hmmAcc:
+                        tagger = "Tagger: HMM   | "
+                        testFile = fileList[i]
+                        a = str(acc)
+                        write = tagger + "Trained on: " + file + " | Tested on: " + testFile + " | Accuracy: " + a
+                        results.write(write + "\n")
+                        i += 1
+
+                    i = 0
+                    for acc in brillAcc:
+                        tagger = "Tagger: Brill | "
+                        testFile = fileList[i]
+                        a = str(acc)
+                        write = tagger + "Trained on: " + file + " | Tested on: " + testFile + " | Accuracy: " + a
+                        results.write(write + "\n")
+                        i += 1
+
+            else:
+                print("Incorrect usage: mode must be either --stanford or --nltk")
+
+
+
+if __name__ == '__main__':
+    #check command line arguments
+    if len(sys.argv) == 3:
+        main(*sys.argv[1:])
+    else:
+        print("Usage: ./main.py <--stanford or --nltk> <directory to model>\n")
---
+++ final/code/models.py
@@ -0,0 +1,79 @@
+import subprocess
+from os import remove
+from nltk.tag.hmm import HiddenMarkovModelTagger
+from nltk.tag.brill_trainer import BrillTaggerTrainer
+from nltk.tag import brill
+class NltkModel(object):
+    def __init__(self, **kwargs):
+        self.directory = kwargs["directory"]
+
+class HMM(NltkModel):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.hmm = HiddenMarkovModelTagger
+
+    def train(self, text):
+        self.hmmTrained = self.hmm.train(text)
+        return self.hmmTrained
+
+    def test(self, testText):
+        acc = self.hmmTrained.evaluate(testText)
+        return acc
+
+class Brill(NltkModel):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tagger = kwargs["tagger"]
+
+        templates = [
+            brill.Template(brill.Pos([-1])),
+            brill.Template(brill.Pos([1])),
+            brill.Template(brill.Pos([-2])),
+            brill.Template(brill.Pos([2])),
+            brill.Template(brill.Pos([-2, -1])),
+            brill.Template(brill.Pos([1, 2])),
+            brill.Template(brill.Pos([-3, -2, -1])),
+            brill.Template(brill.Pos([1, 2, 3])),
+            brill.Template(brill.Pos([-1]), brill.Pos([1])),
+            brill.Template(brill.Word([-1])),
+            brill.Template(brill.Word([1])),
+            brill.Template(brill.Word([-2])),
+            brill.Template(brill.Word([2])),
+            brill.Template(brill.Word([-2, -1])),
+            brill.Template(brill.Word([1, 2])),
+            brill.Template(brill.Word([-3, -2, -1])),
+            brill.Template(brill.Word([1, 2, 3])),
+            brill.Template(brill.Word([-1]), brill.Word([1])),
+            ]
+
+        self.brillTrainer = BrillTaggerTrainer(self.tagger, templates, deterministic=True)
+    def train(self, text):
+        self.trainedBrill = self.brillTrainer.train(text)
+
+    def test(self, testText):
+        acc = self.trainedBrill.evaluate(testText)
+        return acc
+
+class StanfordModel(object):
+    def __init__(self, fileName, directory):
+        self.fileName = fileName
+        self.directory = directory
+
+    def train(self):
+        bash_script = "java -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -prop ./myPropsFile.prop -model "
+        bash_script += self.fileName
+        bash_script += " -trainFile "
+        bash_script += self.directory
+
+        process = subprocess.Popen(bash_script.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+    def test(self, testFile):
+        if "Test" in testFile:
+            bash_script = "java -classpath stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -prop ./myPropsFile.prop -model "
+            bash_script += self.fileName + " -testFile " + testFile
+
+            process = subprocess.Popen(bash_script.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
+            return output
